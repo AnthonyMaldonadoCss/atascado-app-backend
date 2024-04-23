e0f5391257a8b569e927801680ee77ca9ef60f95
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-04-23 16:49:09 -0400

    fix: applied relative imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,27 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TaskModule } from './task/task.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { BuildingsModule } from './buildings/buildings.module';
-import { OrganizationsModule } from './organizations/organizations.module';
-import { NeighborsModule } from './neighbors/neighbors.module';
-import { VehiclesModule } from './vehicles/vehicles.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { TaskModule } from './task/task.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { UserModule } from './user/user.module';
+// import { BuildingsModule } from './buildings/buildings.module';
+// import { OrganizationsModule } from './organizations/organizations.module';
+// import { NeighborsModule } from './neighbors/neighbors.module';
+// import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: ['.env.development'], isGlobal: true }),
-    MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: process.env.CONNECTION_STRING,
-      }),
-    }),
-    TaskModule,
-    UserModule,
-    BuildingsModule,
-    OrganizationsModule,
-    NeighborsModule,
-    VehiclesModule,
+    // ConfigModule.forRoot({ envFilePath: ['.env.development'], isGlobal: true }),
+    // MongooseModule.forRootAsync({
+    //   useFactory: () => ({
+    //     uri: process.env.CONNECTION_STRING,
+    //   }),
+    // }),
+    // TaskModule,
+    // UserModule,
+    // BuildingsModule,
+    // OrganizationsModule,
+    // NeighborsModule,
+    // VehiclesModule,
   ],
 })
 export class AppModule {}
